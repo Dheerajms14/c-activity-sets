0963bd1857f43877b0a12b3d2cebefdccb87a6f8
Dheeraj <dheerajms.bsc23@rvu.edu.in>
2024-02-17 19:29:43 +0530

    test

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+
 int main() {
     int n = input_array_size();
     int array[n];
@@ -38,7 +43,7 @@ int is_composite(int num) {
         }
     }
     
-    return 0;
+    return 0; 
 }
 
 int sum_composite_numbers(int n, int a[n]) {
