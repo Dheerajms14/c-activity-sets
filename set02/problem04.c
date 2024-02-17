2dc3239ad0af14d8baeada3319111efc44a266f8
Dheeraj <dheerajms.bsc23@rvu.edu.in>
2024-02-17 19:17:20 +0530

    test

---
+++ set02/problem04.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main() {
+    int n = input_array_size();
+    int array[n];
+    
+    input_array(n, array);
+    
+    int result = sum_composite_numbers(n, array);
+    
+    output(result);
+    
+    return 0;
+}
+
+int input_array_size() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    return n;
+}
+
+void input_array(int n, int a[n]) {
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+}
+
+int is_composite(int num) {
+    if (num < 4) {
+        return 0;  
+    }
+    
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return 1;  
+        }
+    }
+    
+    return 0;
+}
+
+int sum_composite_numbers(int n, int a[n]) {
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (is_composite(a[i])) {
+            sum += a[i];
+        }
+    }
+    
+    return sum;
+}
+
+void output(int sum) {
+    printf("Sum of composite numbers: %d\n", sum);
+}
